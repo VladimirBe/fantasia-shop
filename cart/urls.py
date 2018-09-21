2bab0d64c4d08e9c1ef80bb70939689048735734
vladimirbe <bejanvadim@yahoo.com>
2018-09-21 10:01:54 +0000

    Third stable debuged,added static

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
-
+app_name = "cart"
 urlpatterns = [
-    url(r'^$', views.CartDetail, name='CartDetail'),
-    url(r'^remove/(?P<product_id>\d+)/$', views.CartRemove, name='CartRemove'),
-    url(r'^add/(?P<product_id>\d+)/$', views.CartAdd, name='CartAdd'),
+    re_path(r'^$', views.CartDetail, name='CartDetail'),
+    re_path(r'^remove/(?P<product_id>\d+)/$', views.CartRemove, name='CartRemove'),
+    re_path(r'^add/(?P<product_id>\d+)/$', views.CartAdd, name='CartAdd'),
 ]
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 # Модель категории
 class Category(models.Model):
